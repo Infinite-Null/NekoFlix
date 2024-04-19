71dbffc2e53ca0e99222944364e01dc89c1ef636
Infinite-Null <ankit.kum.sha9933@gmail.com>
2024-04-20 02:45:00 +0530

    Splash screen

--- Route/InitialRoute.jsx
+++ Route/InitialRoute.jsx
@@ -1,5 +1,5 @@
 import Animated, { FadeIn } from "react-native-reanimated";
-import { View } from "react-native";
+import { Dimensions, StatusBar, View } from "react-native";
 import { MainWrapper } from "../Layout/MainWrapper";
 import { useTheme } from "@react-navigation/native";
 import { useEffect } from "react";
@@ -8,6 +8,7 @@ import { GetHomePage } from "../LocalStorage/AppSettings";
 
 export const InitialScreen = ({navigation}) => {
   const theme = useTheme()
+  const {width, height} = Dimensions.get("window")
   async function InitialCall(){
     const screen = await GetHomePage()
     if (screen === "Anime"){
@@ -21,24 +22,34 @@ export const InitialScreen = ({navigation}) => {
   }, []);
   return (
     <MainWrapper>
+      <StatusBar translucent={true}/>
       <View style={{
         flex:1,
         alignItems:"center",
         justifyContent:"center",
       }}>
-        <FastImage source={require("../assets/AppImages/nekoJump.gif")} style={{
-          height:200,
-          width:200,
+        <FastImage source={require("../assets/AppImages/splash.gif")}  style={{
+          height:height,
+          width:width,
         }} resizeMode={FastImage.resizeMode.stretch}/>
-        <Animated.Text entering={FadeIn.delay(100).duration(300)} style={{
-          fontSize:40,
-          color:theme.colors.text,
-          fontWeight:500,
-        }}>Nekoflix</Animated.Text>
-        <Animated.Text entering={FadeIn.delay(300)} style={{
-          fontSize:15,
-          color:theme.colors.primary,
-        }}>Anime for you.</Animated.Text>
+        <View style={{
+          position:"absolute",
+          width:width,
+          height:height,
+          alignItems:"center",
+          justifyContent:"center",
+          backgroundColor:"rgba(0,0,0,0.86)",
+        }}>
+          <Animated.Text entering={FadeIn.delay(100).duration(300)} style={{
+            fontSize:30,
+            color:'white',
+            fontWeight:500,
+          }}>Nekoflix</Animated.Text>
+          <Animated.Text entering={FadeIn.delay(300)} style={{
+            fontSize:15,
+            color:theme.colors.primary,
+          }}>One app you need</Animated.Text>
+        </View>
       </View>
     </MainWrapper>
   );
